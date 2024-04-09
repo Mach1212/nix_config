8ce352eb387c3987903763c6274554f58b652b9a
Mach1212 <maciej.pruchnik@gmail.com>
2024-04-09 17:22:50 -0400

    [Feat] idk

--- flake.nix
+++ flake.nix
@@ -4,23 +4,37 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixos-wsl.url = "github:nix-community/nixos-wsl";
-
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, nixos-wsl, ... }@inputs: {
-    nixosConfigurations = {
-      default = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs;};
-        modules = [
-          nixos-wsl.nixosModules.wsl
-          ./hosts/default/configuration.nix
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations =
+      let
+        defaultModules = [
           inputs.home-manager.nixosModules.default
         ];
+      in
+      {
+
+        default = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs; };
+          system = "x86_64-linux";
+          modules = defaultModules // [
+            nixos-wsl.nixosModules.wsl
+            ./hosts/default/configuration.nix
+          ];
+        };
+        laptop = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs; };
+          system = "x86_64-linux";
+          modules = defaultModules // [
+            nixos-wsl.nixosModules.wsl
+            ./hosts/laptop/configuration.nix
+          ];
+        };
       };
-    };
   };
 }
