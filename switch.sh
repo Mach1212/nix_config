f9f18688c1620e2b6259534ed27e90654aa5c3cf
Mach1212 <maciej.pruchnik@gmail.com>
2024-04-14 02:43:48 +0000

    [Bot] Automated commit

--- switch.sh
+++ switch.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-git add * \
+sudo git add * \
   && (sudo nixos-rebuild switch --flake .#"$1" || (echo "Failed build" && exit 1)) \
-	&& git commit -am "Automated commit" \
+	&& git commit -am "[Bot] Automated commit" \
 	&& git push
