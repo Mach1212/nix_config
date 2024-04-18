dcb8a0c9d62d075d05d6e348a2e8b12375f7360d
Mach1212 <maciej.pruchnik@gmail.com>
2024-04-18 22:20:47 +0000

    [Bot] Automated commit

--- modules/programs/ssh.nix
+++ modules/programs/ssh.nix
@@ -1,20 +1,9 @@
 { config, pkgs, primaryUser, ... }:
 
 {
-  # services.sshd.enable = true;
   services.openssh = {
     enable = true;
     settings.PasswordAuthentication = false;
     settings.KbdInteractiveAuthentication = false;
   };
-
-  # home-manager.users."${primaryUser}" = {
-  #   programs.ssh = {
-  #     enable = true;
-  #     extraOptionOverrides = {
-  #       PasswordAuthentication = "no";
-  #       PubkeyAuthentication = "yes";
-  #     };
-  #   };
-  # };
 }
