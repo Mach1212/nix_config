138a392c92b22d195ae1b48f1eadcbe2ad7cab8c
Mach1212 <maciej.pruchnik@gmail.com>
2024-04-18 19:54:13 +0000

    [Bot] Automated commit

--- modules/programs/kubernetes.nix
+++ modules/programs/kubernetes.nix
@@ -11,6 +11,13 @@
       bashrcExtra = ''
         source <(kubectl completion bash)
         complete -o default -F __start_kubectl kc
+        complete -o default -F __start_kubectl kg
+        complete -o default -F __start_kubectl ka
+        complete -o default -F __start_kubectl kd
+        complete -o default -F __start_kubectl kl
+        complete -o default -F __start_kubectl klf
+        complete -o default -F __start_kubectl ke
+        complete -o default -F __start_kubectl kdesc
       '';
       shellAliases = {
         ic = "istioctl";
