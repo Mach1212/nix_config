30eb414ab750e87f53beda6313151de48e2d7692
Mach1212 <maciej.pruchnik@gmail.com>
2024-04-18 22:45:28 +0000

    [Bot] Automated commit

--- flake.nix
+++ flake.nix
@@ -17,11 +17,13 @@
         wslModules = [
           nixos-wsl.nixosModules.wsl
           ./modules/programs/wsl.nix
+          ./hosts/default_configuration.nix
+          ./hosts/wsl/hardware-configuration.nix
         ];
         defaultModules = [
           home-manager.nixosModules.home-manager
+          ./hosts/users.nix
           ./modules/programs/home-manager.nix
-          ./modules/users
           ./modules/programs/bash.nix
           ./modules/programs/git.nix
           ./modules/programs/neovim.nix
@@ -35,7 +37,6 @@
             specialArgs = { inherit inputs; primaryUser = "mach12"; };
             system = "x86_64-linux";
             modules = defaultModules ++ wslModules ++ [
-              ./hosts/default/configuration.nix
               ./modules/programs/kubernetes.nix
               ./modules/programs/ssh.nix
             ];
@@ -46,7 +47,6 @@
             system = "x86_64-linux";
             modules = defaultModules ++ wslModules ++ [
               nixos-wsl.nixosModules.wsl
-              ./hosts/laptop/configuration.nix
               ./modules/programs/kubernetes.nix
             ];
           };
