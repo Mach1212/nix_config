4819c8fa7595ee366a634d92ad3c7ce55e2ae46f
Mach1212 <maciej.pruchnik@gmail.com>
2024-04-18 17:23:38 +0000

    [Bot] Automated commit

--- modules/programs/neovim.nix
+++ modules/programs/neovim.nix
@@ -33,12 +33,62 @@
 
     programs.bash = {
       bashrcExtra = ''
+        if [[ -z "$ZELLIJ" ]]; then
+        	if [[ "$ZELLIJ_AUTO_ATTACH" == "true" ]]; then
+        		zellij attach -c
+        	else
+        		zellij a
+        	fi
+
+        	if [[ "$ZELLIJ_AUTO_EXIT" == "true" ]]; then
+        		exit
+        	fi
+        fi
+
         npm set prefix $HOME/.npm-global
         
         export PATH=$HOME/.npm-global/bin:$PATH
         export PATH=$HOME/.pip-global/bin:$PATH
         export PYTHONPATH='/home/${primaryUser}/.pip-global/lib/python3.11/site-packages/'
+        export PATH=$HOME/scripts:$PATH
       '';
+      shellAliases = {
+        python = "python3";
+        docker = "sudo docker";
+        dc = "docker compose";
+        dk = "docker kill";
+        dp = "docker ps";
+        db = "docker build";
+        dr = "docker run";
+        await = ''function await_command() { until $1; do sleep $2; echo Attempting...; done; }; await_command'';
+
+        st = "ssh_to.sh";
+        cn = "kubectl config set-context --current --namespace";
+        mkmvn = ''function make_maven_proj() { if [[ -z "$1" ]]; then printf "USAGE: mkmvn <PROJECT_NAME> <GROUP_ID>\n             PROJECT_NAME: awesome_app\n             GROUP_ID    : com.mach12.app"; return; fi; local GROUP_ID="$2"; if [[ -z "$2" ]]; then GROUP_ID=com.mach12.app; fi; mvn archetype:generate -DgroupId=$GROUP_ID -DartifactId=$1 -DarchetypeArtifactId=maven-archetype-quickstart -DarchetypeVersion=1.4 -DinteractiveMode=false; }; make_maven_proj'';
+        cmakeDev = "cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1";
+        genLLVMIR = "clang -S -emit-llvm";
+
+        ic = "istioctl";
+        kc = "kubectl";
+        kg = "kc get";
+        ka = "kc apply";
+        kaf = "ka -f";
+        kr = ''function make_pod() { kc run "$1" --image "$2" "''${@:3}"; }; make_pod'';
+        krOn = "krOn.sh";
+        kd = "kc delete";
+        kdf = "kd -f";
+        kl = "kc logs";
+        klf = "kc logs --follow";
+        ke = "kc edit";
+        kDbg = ''function debug_faulty_pod() {  kc debug "$1" --image=ubuntu --share-processes --copy-to debug-"$1" -it -- sh && kd pod/debug-"$1"; }; debug_faulty_pod'';
+        kdesc = "kc describe";
+        kTestDns = ''kubectl run dns-tester --image=busybox --rm --attach --command -- sh -c "cat /etc/resolv.conf; nslookup $POD.$NAMESPACE.pod.cluster.local"'';
+        kDbgDns = ''kubectl run -it --rm --image=infoblox/dnstools dns-client'';
+        kdJobs = ''kubectl delete jobs `kubectl get jobs -o custom-columns=:.metadata.name`'';
+        kdfailed = ''kubectl delete pods --field-selector status.phase=Failed'';
+
+        disasm = "objdump -zd";
+      };
     };
 
     programs.neovim = {
