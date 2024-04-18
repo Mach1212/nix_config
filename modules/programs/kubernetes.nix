9e987eb68e19047427b0219ef55da3d022f659fe
Mach1212 <maciej.pruchnik@gmail.com>
2024-04-18 19:09:48 +0000

    [Bot] Automated commit

--- modules/programs/kubernetes.nix
+++ modules/programs/kubernetes.nix
@@ -6,5 +6,29 @@
       pkgs.kubectl
       pkgs.kubernetes-helm
     ];
+
+    programs.bash = {
+      shellAliases = {
+        ic = "istioctl";
+        kc = "kubectl";
+        kubectl = ''echo 'complete -o default -F __start_kubectl k' >>~/.bashrc'';
+        kg = "kc get";
+        ka = "kc apply";
+        kaf = "ka -f";
+        kr = ''function make_pod() { kc run "$1" --image "$2" "''${@:3}"; }; make_pod'';
+        krOn = "krOn.sh";
+        kd = "kc delete";
+        kdf = "kd -f";
+        kl = "kc logs";
+        klf = "kc logs --follow";
+        ke = "kc edit";
+        kDbg = ''function debug_faulty_pod() {  kc debug "$1" --image=ubuntu --share-processes --copy-to debug-"$1" -it -- sh && kd pod/debug-"$1"; }; debug_faulty_pod'';
+        kdesc = "kc describe";
+        kTestDns = ''kubectl run dns-tester --image=busybox --rm --attach --command -- sh -c "cat /etc/resolv.conf; nslookup $POD.$NAMESPACE.pod.cluster.local"'';
+        kDbgDns = ''kubectl run -it --rm --image=infoblox/dnstools dns-client'';
+        kdJobs = ''kubectl delete jobs `kubectl get jobs -o custom-columns=:.metadata.name`'';
+        kdfailed = ''kubectl delete pods --field-selector status.phase=Failed'';
+      };
+    };
   };
 }
--- modules/programs/neovim.nix
+++ modules/programs/neovim.nix
@@ -71,25 +71,6 @@
         cmakeDev = "cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1";
         genLLVMIR = "clang -S -emit-llvm";
 
-        ic = "istioctl";
-        kc = "kubectl";
-        kg = "kc get";
-        ka = "kc apply";
-        kaf = "ka -f";
-        kr = ''function make_pod() { kc run "$1" --image "$2" "''${@:3}"; }; make_pod'';
-        krOn = "krOn.sh";
-        kd = "kc delete";
-        kdf = "kd -f";
-        kl = "kc logs";
-        klf = "kc logs --follow";
-        ke = "kc edit";
-        kDbg = ''function debug_faulty_pod() {  kc debug "$1" --image=ubuntu --share-processes --copy-to debug-"$1" -it -- sh && kd pod/debug-"$1"; }; debug_faulty_pod'';
-        kdesc = "kc describe";
-        kTestDns = ''kubectl run dns-tester --image=busybox --rm --attach --command -- sh -c "cat /etc/resolv.conf; nslookup $POD.$NAMESPACE.pod.cluster.local"'';
-        kDbgDns = ''kubectl run -it --rm --image=infoblox/dnstools dns-client'';
-        kdJobs = ''kubectl delete jobs `kubectl get jobs -o custom-columns=:.metadata.name`'';
-        kdfailed = ''kubectl delete pods --field-selector status.phase=Failed'';
-
         disasm = "objdump -zd";
       };
     };
