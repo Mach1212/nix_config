f1439207d6094d55c99e8d8f00ca3975061c5c7c
Mach1212 <maciej.pruchnik@gmail.com>
2024-04-19 07:29:11 +0000

    [Bot] Automated commit

--- modules/programs/tailscale.nix
+++ modules/programs/tailscale.nix
@@ -4,13 +4,4 @@
   services.tailscale = {
     enable = true;
   };
-
-  specialisation.github.configuration = {
-    services.tailscale = {
-      extraUpFlags = [
-        ""
-      ];
-    };
-  };
-
 }
