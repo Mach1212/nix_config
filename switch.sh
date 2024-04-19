e4629bcc48098479fc5a5edab5965798b62539e4
Mach1212 <maciej.pruchnik@gmail.com>
2024-04-19 03:23:11 +0000

    [Bot] Automated commit

--- switch.sh
+++ switch.sh
@@ -8,6 +8,5 @@ fi
 sudo chown -R $USER .git &&
 	sudo git add * &&
 	git commit -am "[Bot] Automated commit" &&
-	sudo git pull &&
 	(sudo nixos-rebuild switch --flake .#"$1" || exit 1) &&
 	git push
