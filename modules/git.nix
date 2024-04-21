b4e7c743f0606c5ff9768e97e12d05b404893e36
Mach1212 <maciej.pruchnik@gmail.com>
2024-04-21 00:36:04 +0000

    [Bot] Automated commit

--- modules/git.nix
+++ modules/git.nix
@@ -1,26 +1,29 @@
-{ config, pkgs, primaryUser, ... }:
+{ config, pkgs, lib, primaryUser, ... }:
 
 {
   home-manager.users."${primaryUser}" = {
     home.packages = [
       pkgs.gh
     ];
-    programs.git = {
-      enable = true;
-      extraConfig = {
-        pull = { rebase = true; };
-        rebase = { autoStash = true; };
-      };
-    } // {
-      "mach12" = {
-        userName = "Mach1212";
-        userEmail = "maciej.pruchnik@gmail.com";
+    programs.git = lib.mkMerge [
+      {
+        enable = true;
         extraConfig = {
-          credential = { helper = "store"; };
-          branch = { autosetuprebase = "always"; };
+          pull = { rebase = true; };
+          rebase = { autoStash = true; };
         };
-      };
-      "nixos" = { };
-    }."${primaryUser}";
+      }
+      {
+        "mach12" = {
+          userName = "Mach1212";
+          userEmail = "maciej.pruchnik@gmail.com";
+          extraConfig = {
+            credential = { helper = "store"; };
+            branch = { autosetuprebase = "always"; };
+          };
+        };
+        "nixos" = { };
+      }."${primaryUser}"
+    ];
   };
 }
