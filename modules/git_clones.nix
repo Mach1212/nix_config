3c36a5585444aee88a8cd2fb0a5fcda3fb35234b
Mach1212 <maciej.pruchnik@gmail.com>
2024-04-21 00:19:10 +0000

    [Bot] Automated commit

--- modules/git_clones.nix
+++ modules/git_clones.nix
@@ -2,11 +2,10 @@
 
 {
   system.userActivationScripts.setup_clones.text = ''
-    if [! -d "~/.config/astro_config"]; then
+    if [! -d "~/astro_config"]; then
       mkdir ~/wut
       echo '${inputs.astro-config}' >~/inputs
       cp ${inputs.astro-config} '~/.config/astro_config'
     fi
-    mkdir ~/hi
   '';
 }
