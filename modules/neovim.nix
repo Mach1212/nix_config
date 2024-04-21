a8ba670d2a899f2bacc379c486351313515ded63
Mach1212 <maciej.pruchnik@gmail.com>
2024-04-21 06:49:49 +0000

    [Bot] Automated commit

--- modules/neovim.nix
+++ modules/neovim.nix
@@ -31,8 +31,19 @@
       prefix=/home/${primaryUser}/.pip-global
     '';
 
-    programs.bash = {
-      bashrcExtra = ''
+    environment.etc =
+      {
+        sysctl.conf = {
+          text = ''
+            net.ipv6.conf.all.disable_ipv6 = 1 
+            net.ipv6.conf.default.disable_ipv6 = 1
+            net.ipv6.conf.lo.disable_ipv6 = 1
+          '';
+        };
+      }
+
+        programs.bash = {
+    bashrcExtra = ''
         if [[ -z "$ZELLIJ" ]]; then
         	if [[ "$ZELLIJ_AUTO_ATTACH" == "true" ]]; then
         		zellij attach -c
@@ -53,36 +64,36 @@
         export PATH=$HOME/scripts:$PATH
         export NVIM_APPNAME=astro_config
       '';
-      shellAliases = {
-        python = "python3";
-        docker = "sudo docker";
-        dc = "docker compose";
-        dk = "docker kill";
-        dp = "docker ps";
-        db = "docker build";
-        dr = "docker run";
-        await = ''function await_command() { until $1; do sleep $2; echo Attempting...; done; }; await_command'';
+    shellAliases = {
+      python = "python3";
+      docker = "sudo docker";
+      dc = "docker compose";
+      dk = "docker kill";
+      dp = "docker ps";
+      db = "docker build";
+      dr = "docker run";
+      await = ''function await_command() { until $1; do sleep $2; echo Attempting...; done; }; await_command'';
 
-        st = "ssh_to.sh";
-        cn = "kubectl config set-context --current --namespace";
-        mkmvn = ''function make_maven_proj() { if [[ -z "$1" ]]; then printf "USAGE: mkmvn <PROJECT_NAME> <GROUP_ID>\n             PROJECT_NAME: awesome_app\n             GROUP_ID    : com.${primaryUser}.app"; return; fi; local GROUP_ID="$2"; if [[ -z "$2" ]]; then GROUP_ID=com.${primaryUser}.app; fi; mvn archetype:generate -DgroupId=$GROUP_ID -DartifactId=$1 -DarchetypeArtifactId=maven-archetype-quickstart -DarchetypeVersion=1.4 -DinteractiveMode=false; }; make_maven_proj'';
-        cmakeDev = "cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1";
-        genLLVMIR = "clang -S -emit-llvm";
+      st = "ssh_to.sh";
+      cn = "kubectl config set-context --current --namespace";
+      mkmvn = ''function make_maven_proj() { if [[ -z "$1" ]]; then printf "USAGE: mkmvn <PROJECT_NAME> <GROUP_ID>\n             PROJECT_NAME: awesome_app\n             GROUP_ID    : com.${primaryUser}.app"; return; fi; local GROUP_ID="$2"; if [[ -z "$2" ]]; then GROUP_ID=com.${primaryUser}.app; fi; mvn archetype:generate -DgroupId=$GROUP_ID -DartifactId=$1 -DarchetypeArtifactId=maven-archetype-quickstart -DarchetypeVersion=1.4 -DinteractiveMode=false; }; make_maven_proj'';
+      cmakeDev = "cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1";
+      genLLVMIR = "clang -S -emit-llvm";
 
-        disasm = "objdump -zd";
-      };
-    };
-
-    programs.neovim = {
-      enable = true;
-      defaultEditor = true;
-      viAlias = true;
-      vimAlias = true;
-      vimdiffAlias = true;
-    };
-    xdg.configFile."nvim".source = builtins.fetchGit {
-      url = "https://github.com/Mach1212/astro_config";
-      rev = "f3ccb0e7ff3bd4a590e7ae3496caacbe6b53b0d6";
+      disasm = "objdump -zd";
     };
   };
+
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+  };
+  xdg.configFile."nvim".source = builtins.fetchGit {
+    url = "https://github.com/Mach1212/astro_config";
+    rev = "f3ccb0e7ff3bd4a590e7ae3496caacbe6b53b0d6";
+  };
+};
 }
