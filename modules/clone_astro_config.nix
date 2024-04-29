20f4e29f8dcc96b8b146b5932ffa4b68c3aec98b
Mach1212 <maciej.pruchnik@gmail.com>
2024-04-29 19:39:31 +0000

    [Bot] Automated commit

--- modules/clone_astro_config.nix
+++ modules/clone_astro_config.nix
@@ -9,11 +9,11 @@
       if [ ! -d ${path} ]; then
         rm -rf ~/here*
         cp -r ${inputs.astro-config} ${path}
-        mkdir ~/here
+        # mkdir ~/here
         # sudo chown -R ${primaryUser} ${path}
-        mkdir ~/here2
-        ${pkgs.uutils-coreutils-noprefix.chmod} -R a-rwx ${path}
-        mkdir ~/here3
+        # mkdir ~/here2
+        # chmod -R a-rwx ${path}
+        # mkdir ~/here3
       fi
     '';
 }
