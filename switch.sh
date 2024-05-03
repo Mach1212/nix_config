7b09cb59782d9eade2483d63f9a7e47103bd2b38
Mach1212 <maciej.pruchnik@gmail.com>
2024-05-03 23:10:43 +0000

    [Bot] Automated commit

--- switch.sh
+++ switch.sh
@@ -4,9 +4,9 @@ if [[ -n $2 ]]; then
 	specialization="--specialisation $2"
 fi
 
-git add '*' || exit 1
-git diff-index --quiet HEAD || git commit -am "[Bot] Automated commit" || exit 1
 nix flake lock --update-input astro-config || exit 1
 nix flake lock --update-input nix-config || exit 1
+git add '*' || exit 1
+git diff-index --quiet HEAD || git commit -am "[Bot] Automated commit" || exit 1
 sudo nixos-rebuild switch --flake .#"$1" $specialization || exit 1
 git push
