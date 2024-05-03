5d21c92fbd9e4c2719f402c24d49f0fa80b7da3e
Mach1212 <maciej.pruchnik@gmail.com>
2024-05-03 23:12:00 +0000

    [Bot] Automated commit

--- switch.sh
+++ switch.sh
@@ -4,7 +4,9 @@ if [[ -n $2 ]]; then
 	specialization="--specialisation $2"
 fi
 
+echo Updating astro-config
 nix flake lock --update-input astro-config || exit 1
+echo Updating nix-config
 nix flake lock --update-input nix-config || exit 1
 git add '*' || exit 1
 git diff-index --quiet HEAD || git commit -am "[Bot] Automated commit" || exit 1
