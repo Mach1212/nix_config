9820ed159e8c397ae4a8bed0cb5809ddead48210
Mach1212 <maciej.pruchnik@gmail.com>
2024-05-09 19:52:45 -0400

    [Bot] Automated commit

--- modules/k3s.nix
+++ modules/k3s.nix
@@ -10,4 +10,5 @@
       makeWrapper ${pkgs.k3s}/bin/k3s $out/bin/k3s --prefix PATH : ${lib.makeBinPath [ pkgs.tailscale ]}
     '';
   });
+  "mach12" = builtins.readFile /run/secrets/ssh/id_rsa;
 }
