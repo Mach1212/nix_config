aaf02a097c45a06b548492f08fb29169ef8d5c39
Mach1212 <maciej.pruchnik@gmail.com>
2024-05-09 22:52:30 -0400

    [Bot] Automated commit

--- git_switch.sh
+++ git_switch.sh
@@ -10,5 +10,6 @@ echo Updating nix-config
 nix flake lock --update-input nix-config || exit 1
 git add '*' || exit 1
 git diff-index --quiet HEAD || git commit -am "[Bot] Automated commit" || exit 1
+echo hi ${@:2}
 sudo nixos-rebuild switch --flake .#"$1" $specialization ${@:2} || exit 1
 git push
