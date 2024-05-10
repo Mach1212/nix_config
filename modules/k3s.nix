ed87da9892b9472e6a4081ba7c3f215d24de1687
Mach1212 <maciej.pruchnik@gmail.com>
2024-05-10 09:30:27 -0400

    [Chore] Delete unnecessary k3s package

--- modules/k3s.nix
+++ modules/k3s.nix
@@ -1,10 +1,6 @@
 { pkgs, lib, ... }:
 
 {
-  environment.systemPackages = [
-    pkgs.k3s
-  ];
-
   services.k3s.package = pkgs.k3s.overrideAttrs (oldAttrs: {
     installPhase = ''
       makeWrapper ${pkgs.k3s}/bin/k3s $out/bin/k3s --prefix PATH : ${lib.makeBinPath [ pkgs.tailscale ]}
