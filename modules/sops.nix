4149ef17c7f03aa7d7862f0522ee85959a4bcdf0
Mach1212 <maciej.pruchnik@gmail.com>
2024-05-10 13:09:47 -0400

    [Bot] Automated commit

--- modules/sops.nix
+++ modules/sops.nix
@@ -8,25 +8,29 @@
   ];
   
   home-manager.users."${primaryUser}" = {
-    home.packages = [
-      pkgs.sops
-    ];
-
-    sops.defaultSopsFile = ../secrets/secrets.yaml;
-    sops.defaultSopsFormat = "yaml";
-
-    sops.age.keyFile = "/home/${primaryUser}/.config/sops/age/keys.txt";
-    sops.secrets = {
-      "ssh/id_rsa" = { 
-        owner = "${primaryUser}";
-      };
-      "tailscale" = {
-        owner = "${primaryUser}";
+    home = {
+      packages = [
+        pkgs.sops
+      ];
+      file = {
+        ".ssh/id_rsa".source = builtins.readFile config.sops.secrets."ssh/id_rsa".path;
+        ".secrets/tailscale".source = builtins.readFile config.sops.secrets."tailscale".path;
       };
     };
 
-    
-    home.file.".ssh/id_rsa".source = builtins.readFile config.sops.secrets."ssh/id_rsa".path;
-    home.file.".secrets/tailscale".source = builtins.readFile config.sops.secrets."tailscale".path;
+    sops = {
+      defaultSopsFile = ../secrets/secrets.yaml;
+      defaultSopsFormat = "yaml";
+
+      age.keyFile = "/home/${primaryUser}/.config/sops/age/keys.txt";
+      secrets = {
+        "ssh/id_rsa" = { 
+          owner = "${primaryUser}";
+        };
+        "tailscale" = {
+          owner = "${primaryUser}";
+        };
+      };
+    };
   };
 }
