8a96d63a015c879a88391756b0f156a7cebb518d
Mach1212 <maciej.pruchnik@gmail.com>
2024-05-10 14:18:02 -0400

    [Bot] Automated commit

--- modules/sops.nix
+++ modules/sops.nix
@@ -13,7 +13,7 @@
         pkgs.sops
       ];
       file = {
-        ".ssh/id_rsa".source = builtins.readFile config.sops.secrets."ssh/id_rsa".path;
+        # ".ssh/id_rsa".source = builtins.readFile config.sops.secrets."ssh/id_rsa".path;
         # ".secrets/tailscale".source = builtins.readFile config.sops.secrets."tailscale".path;
       };
     };
