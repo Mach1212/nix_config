9be719b1000b43c585cff8897c3f05a423234503
Mach1212 <maciej.pruchnik@gmail.com>
2024-05-10 13:07:39 -0400

    [Bot] Automated commit

--- modules/sops.nix
+++ modules/sops.nix
@@ -2,24 +2,29 @@
 
 {
   imports = [ inputs.sops-nix.nixosModules.sops ];
-  sops.defaultSopsFile = ../secrets/secrets.yaml;
-  sops.defaultSopsFormat = "yaml";
-
-  sops.age.keyFile = "/home/${primaryUser}/.config/sops/age/keys.txt";
-  sops.secrets = {
-    "ssh/id_rsa" = { 
-      owner = "${primaryUser}";
-    };
-    "tailscale" = {
-      owner = "${primaryUser}";
-    };
-  };
+  
+  home-manager.sharedModules = [
+    inputs.sops-nix.homeManagerModules.sops
+  ];
   
   home-manager.users."${primaryUser}" = {
-
     home.packages = [
       pkgs.sops
     ];
+
+    sops.defaultSopsFile = ../secrets/secrets.yaml;
+    sops.defaultSopsFormat = "yaml";
+
+    sops.age.keyFile = "/home/${primaryUser}/.config/sops/age/keys.txt";
+    sops.secrets = {
+      "ssh/id_rsa" = { 
+        owner = "${primaryUser}";
+      };
+      "tailscale" = {
+        owner = "${primaryUser}";
+      };
+    };
+
     
     home.file.".ssh/id_rsa".source = builtins.readFile config.sops.secrets."ssh/id_rsa".path;
     home.file.".secrets/tailscale".source = builtins.readFile config.sops.secrets."tailscale".path;
