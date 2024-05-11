329b435205d2f6c776f4e0b39d7f3a4b87d41bdb
Mach1212 <maciej.pruchnik@gmail.com>
2024-05-10 21:17:43 -0400

    [Bot] Automated commit

--- switch.sh
+++ switch.sh
@@ -1,7 +1,10 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-if [[ -n $2 ]]; then
-	specialization="--specialisation $2"
-fi
-
-sudo nixos-rebuild switch --flake .#"$1" $specialization ${@:2}
+echo Updating astro-config
+nix flake lock --update-input astro-config || exit 1
+echo Updating nix-config
+nix flake lock --update-input nix-config || exit 1
+git add '*' || exit 1
+git diff-index --quiet HEAD || git commit -am "[Bot] Automated commit" || exit 1
+sudo nixos-rebuild switch --flake .#${@:1} || exit 1
+git push
