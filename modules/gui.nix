659c9e2ebe46d1cef2861eb34eda73799df0f731
Mach1212 <maciej.pruchnik@gmail.com>
2024-05-15 16:37:15 -0400

    [Bot] Automated commit

--- modules/gui.nix
+++ modules/gui.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, primaryUser, ... }:
+{ config, pkgs, primaryUser, stdenvNoCC,  ... }:
 
 {
   nixpkgs.overlays = [
@@ -51,6 +51,22 @@
   services.udev.packages = with pkgs; [ gnome.gnome-settings-daemon ];
 
   home-manager.users."${primaryUser}" = {
+    gtk.iconTheme = {
+      name = "mine";
+      package = stdenvNoCC.mkDerivation {
+        pname = "Win11 Icons";
+        version = "0.5";
+        src = fetchGit {
+          url = "https://github.com/yeyushengfan258/Win11-icon-theme";
+          hash = "9c69f73b00fdaadab946d0466430a94c3e53ff68";
+        };
+        buildInputs = [ pkgs.bash ];
+        installPhase = ''
+          patchShebangs install.sh
+          ./install.sh
+        '';
+      };
+    };
     dconf = {
       enable = true;
       settings = {
