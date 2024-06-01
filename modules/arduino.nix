9786141c5b4f682586c1e8907847d1fccee24e5b
Mach1212 <maciej.pruchnik@gmail.com>
2024-05-31 20:45:12 -0400

    [Bot] Automated commit

--- modules/arduino.nix
+++ modules/arduino.nix
@@ -4,12 +4,6 @@
   home-manager.users."${primaryUser}" = {
     home.packages = [
       pkgs.arduino-ide
-      (pkgs.python312.withPackages
-        (python-pkgs: with python-pkgs; [
-          pyserial
-        ]))
     ];
   };
-
-
 }
--- modules/python.nix
+++ modules/python.nix
@@ -17,6 +17,7 @@
         requests
         matplotlib
         seaborn
+        pyserial
       ]))
     ];
 
