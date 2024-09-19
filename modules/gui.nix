56ee43d51f2b3506b017d58e699b61704f0d6536
Mach1212 <maciej.pruchnik@gmail.com>
2024-09-18 23:17:32 -0400

    [Bot] Automated commit

--- modules/gui.nix
+++ modules/gui.nix
@@ -31,24 +31,26 @@
   services.sysprof.enable = true;
 
   environment = {
-    gnome.excludePackages =
-      (with pkgs; [
-        gnome-photos
-        gnome-tour
-        gedit # text editor
-        xterm
-      ])
-      ++ (with pkgs.gnome; [
-        # gnome-shell-extensions
-        # geary # email reader
-        evince # document viewer
-        gnome-characters
-        totem # video player
-        tali # poker game
-        iagno # go game
-        hitori # sudoku game
-        atomix # puzzle game
-      ]);
+    gnome.excludePackages = with pkgs; [
+      gnome-photos
+      gnome-tour
+      gedit # text editor
+      xterm
+    ];
+    # ++ (with pkgs.gnome; [
+    #   # gnome-shell-extensions
+    #   cheese # webcam tool
+    #   gnome-music
+    #   epiphany # web browser
+    #   # geary # email reader
+    #   evince # document viewer
+    #   gnome-characters
+    #   totem # video player
+    #   tali # poker game
+    #   iagno # go game
+    #   hitori # sudoku game
+    #   atomix # puzzle game
+    # ]);
     systemPackages = with pkgs; [
       sassc
       gnome-menus
