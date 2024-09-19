3d2a3f305279ee9db5b3db2c9085f8703adc5f4a
Mach1212 <maciej.pruchnik@gmail.com>
2024-09-18 22:41:25 -0400

    [Bot] Automated commit

--- flake.nix
+++ flake.nix
@@ -55,10 +55,10 @@
         ++ [
           home-manager.nixosModules.home-manager
           ({primaryUser, ...}: {
-            home-manager.users."${primaryUser}" = {
-              home.username = primaryUser;
-              home.homeDirectory = "/home/${primaryUser}";
-              home.stateVersion = "24.11";
+            home-manager.users."${primaryUser}".home = {
+              username = primaryUser;
+              homeDirectory = "/home/${primaryUser}";
+              stateVersion = "24.11";
             };
           })
           # ./modules/home-manager.nix
