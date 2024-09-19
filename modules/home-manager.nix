79140506dd44272d58f2bc56472828d953136eab
Mach1212 <maciej.pruchnik@gmail.com>
2024-09-18 22:11:37 -0400

    [Bot] Automated commit

--- modules/home-manager.nix
+++ modules/home-manager.nix
@@ -4,7 +4,7 @@
   primaryUser,
   ...
 }: {
-  home-manager.users.${primaryUser} = {
+  home-manager.users.${primaryUser} = {pkgs, ...}: {
     home.username = primaryUser;
     home.homeDirectory = "/home/${primaryUser}";
     home.stateVersion = "24.11";
