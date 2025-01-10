63f71434d103173996bb7fff7447baecaa5ee7f2
Mach1212 <maciej.pruchnik@gmail.com>
2025-01-10 10:12:43 -0500

    [Bot] Automated commit

--- modules/zellij.nix
+++ modules/zellij.nix
@@ -15,21 +15,26 @@
             # "unbind \"Ctrl p\" //" = "";
             # "unbind \"Ctrl t\" //" = "";
             # "unbind \"Ctrl n\" //" = "";
-            # "unbind \"Ctrl h\" //" = "";
             # "unbind \"Ctrl s\" //" = "";
             # "unbind \"Ctrl o\" //" = "";
             # "unbind \"Ctrl q\" //" = "";
+            "unbind \"Ctrl h\" //" = "";
             "unbind \"Alt h\" //" = "";
             "unbind \"Alt l\" //" = "";
             "bind \"Alt g\"" = {SwitchToMode = "locked";};
-            # "bind \"Alt p\"" = {SwitchToMode = "pane";};
-            "bind \"Alt j\"" = {MoveFocusOrTab = "Left";};
-            "bind \"Alt k\"" = {MoveFocusOrTab = "Right";};
+            "bind \"Alt p\"" = {SwitchToMode = "pane";};
+            "bind \"Alt k\"" = {MoveFocusOrTab = "Left";};
+            "bind \"Alt j\"" = {MoveFocusOrTab = "Right";};
+            "bind \"Alt h\"" = {SwitchToMode = "move";};
             # "bind \"Alt h\"" = {SwitchToMode = "move";};
             # "bind \"Alt t\"" = {SwitchToMode = "tab";};
             # "bind \"Alt s\"" = {SwitchToMode = "search";};
             # "bind \"Alt o\"" = {SwitchToMode = "session";};
             # "bind \"Alt q\" {Quit;} //" = "";
+          };
+          move = {
+            "unbind \"Ctrl h\" //" = "";
+            "bind \"Alt h\"" = {SwitchToMode = "normal";};
           };
           locked = {
             "unbind \"Ctrl g\" //" = "";
