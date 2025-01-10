b7493c6fbc21b91fba43e44f82e2fe9d70ab2f42
Mach1212 <maciej.pruchnik@gmail.com>
2025-01-10 09:23:18 -0500

    [Bot] Automated commit

--- modules/zellij.nix
+++ modules/zellij.nix
@@ -12,26 +12,27 @@
         keybinds = {
           normal = {
             "unbind \"Ctrl g\" //" = "";
-            "unbind \"Ctrl p\" //" = "";
-            "unbind \"Ctrl t\" //" = "";
-            "unbind \"Ctrl n\" //" = "";
-            "unbind \"Ctrl h\" //" = "";
-            "unbind \"Ctrl s\" //" = "";
-            "unbind \"Ctrl o\" //" = "";
-            "unbind \"Ctrl q\" //" = "";
+            # "unbind \"Ctrl p\" //" = "";
+            # "unbind \"Ctrl t\" //" = "";
+            # "unbind \"Ctrl n\" //" = "";
+            # "unbind \"Ctrl h\" //" = "";
+            # "unbind \"Ctrl s\" //" = "";
+            # "unbind \"Ctrl o\" //" = "";
+            # "unbind \"Ctrl q\" //" = "";
             "unbind \"Alt h\" //" = "";
             "unbind \"Alt l\" //" = "";
             "bind \"Alt g\"" = {SwitchToMode = "locked";};
-            "bind \"Alt p\"" = {SwitchToMode = "pane";};
+            # "bind \"Alt p\"" = {SwitchToMode = "pane";};
             "bind \"Alt j\"" = {MoveFocusOrTab = "Left";};
             "bind \"Alt k\"" = {MoveFocusOrTab = "Right";};
-            "bind \"Alt h\"" = {SwitchToMode = "move";};
-            "bind \"Alt t\"" = {SwitchToMode = "tab";};
-            "bind \"Alt s\"" = {SwitchToMode = "search";};
-            "bind \"Alt o\"" = {SwitchToMode = "session";};
-            "bind \"Alt q\" {Quit;} //" = "";
+            # "bind \"Alt h\"" = {SwitchToMode = "move";};
+            # "bind \"Alt t\"" = {SwitchToMode = "tab";};
+            # "bind \"Alt s\"" = {SwitchToMode = "search";};
+            # "bind \"Alt o\"" = {SwitchToMode = "session";};
+            # "bind \"Alt q\" {Quit;} //" = "";
           };
           locked = {
+            "unbind \"Ctrl g\" //" = "";
             "bind \"Alt g\"" = {SwitchToMode = "normal";};
           };
         };
