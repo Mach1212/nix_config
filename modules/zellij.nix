33d1d288055f3ef343236f20e141e8eb85ee8cce
Mach1212 <maciej.pruchnik@gmail.com>
2025-01-09 23:00:12 -0500

    [Bot] Automated commit

--- modules/zellij.nix
+++ modules/zellij.nix
@@ -11,10 +11,25 @@
         pane_frames = false;
         keybinds = {
           normal = {
+            "unbind \"Ctrl g\"" = "//";
+            "unbind \"Ctrl p\"" = "//";
+            "unbind \"Ctrl t\"" = "//";
+            "unbind \"Ctrl n\"" = "//";
+            "unbind \"Ctrl h\"" = "//";
+            "unbind \"Ctrl s\"" = "//";
+            "unbind \"Ctrl o\"" = "//";
+            "unbind \"Ctrl q\"" = "//";
             "bind \"Alt g\"" = {SwitchToMode = "locked";};
             "bind \"Alt p\"" = {SwitchToMode = "pane";};
             "bind \"Alt j\"" = {MoveFocusOrTab = "Left";};
             "bind \"Alt k\"" = {MoveFocusOrTab = "Right";};
+            "bind \"Alt h\"" = {SwitchToMode = "move";};
+            "bind \"Alt t\"" = {SwitchToMode = "tab";};
+            "bind \"Alt s\"" = {SwitchToMode = "search";};
+            "bind \"Alt o\"" = {SwitchToMode = "session";};
+            "bind \"Alt q\"" = "{Quit;}";
+            "unbind \"Alt h\"" = "//";
+            "unbind \"Alt l\"" = "//";
           };
           locked = {
             "bind \"Alt g\"" = {SwitchToMode = "normal";};
